95833ae86a32dc00d43cff4ab2cc0dfb462e90b2
José Coelho <contato@josecoelho.com>
2014-09-01 11:06:09 -0300

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name = "bbvisa2ofx",
-    version = "0.18",
+    version = "0.19",
     description = "Converter uma fatura de cartao de credito TXT do Banco do Brasil para OFX",
     author = "Jose Coelho",
     author_email = "contato@josecoelho.com",
